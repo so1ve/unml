30b62f30f081c79ea54179ad37b8a3340b626ddc
so1ve <nn_201312@163.com>
2023-03-25 23:33:11 +0800

    refactor: remove preload

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -1,18 +1,17 @@
 import { release } from "node:os";
 import { join } from "node:path";
 
+import type { WebPreferences } from "electron";
 import { BrowserWindow, app, ipcMain, shell } from "electron";
 
-// The built directory structure
-//
-// ├─┬ dist-electron
-// │ ├─┬ main
-// │ │ └── index.js    > Electron-Main
-// │ └─┬ preload
-// │   └── index.js    > Preload-Scripts
-// ├─┬ dist
-// │ └── index.html    > Electron-Renderer
-//
+const TITLE = "UNML";
+const ICON = join(process.env.PUBLIC, "favicon.ico");
+const WEB_PREFERENCES: WebPreferences = {
+  nodeIntegration: true,
+  nodeIntegrationInSubFrames: true,
+  contextIsolation: false,
+};
+
 process.env.DIST_ELECTRON = join(__dirname, "..");
 process.env.DIST = join(process.env.DIST_ELECTRON, "../dist");
 process.env.PUBLIC = process.env.VITE_DEV_SERVER_URL
@@ -30,29 +29,15 @@ if (!app.requestSingleInstanceLock()) {
   process.exit(0);
 }
 
-// Remove electron security warnings
-// This warning only shows in development mode
-// Read more on https://www.electronjs.org/docs/latest/tutorial/security
-// process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
-
 let win: BrowserWindow | null = null;
-// Here, you can also use other preload
-const preload = join(__dirname, "../preload/index.js");
 const url = process.env.VITE_DEV_SERVER_URL;
 const indexHtml = join(process.env.DIST, "index.html");
 
 function createWindow () {
   win = new BrowserWindow({
-    title: "UNML",
-    icon: join(process.env.PUBLIC, "favicon.ico"),
-    webPreferences: {
-      preload,
-      // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
-      // Consider using contextBridge.exposeInMainWorld
-      // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
-      nodeIntegration: true,
-      contextIsolation: false,
-    },
+    title: TITLE,
+    icon: ICON,
+    webPreferences: WEB_PREFERENCES,
   });
 
   if (process.env.VITE_DEV_SERVER_URL) { // electron-vite-vue#298
@@ -73,7 +58,6 @@ function createWindow () {
     if (url.startsWith("https:")) { shell.openExternal(url); }
     return { action: "deny" };
   });
-  // win.webContents.on('will-navigate', (event, url) => { }) #344
 }
 
 app.whenReady().then(createWindow);
@@ -85,7 +69,6 @@ app.on("window-all-closed", () => {
 
 app.on("second-instance", () => {
   if (win) {
-    // Focus on the main window if the user tried to open another
     if (win.isMinimized()) { win.restore(); }
     win.focus();
   }
@@ -103,11 +86,7 @@ app.on("activate", () => {
 // New window example arg: new windows url
 ipcMain.handle("open-win", (_, arg) => {
   const childWindow = new BrowserWindow({
-    webPreferences: {
-      preload,
-      nodeIntegration: true,
-      contextIsolation: false,
-    },
+    webPreferences: WEB_PREFERENCES,
   });
 
   if (process.env.VITE_DEV_SERVER_URL) {
