251779b7634a36ce9efbe321c6924eabf1d75730
so1ve <nn_201312@163.com>
2023-03-25 23:47:30 +0800

    chore: init kit

--- packages/kit/src/index.ts
+++ packages/kit/src/index.ts
@@ -1,1 +1,15 @@
-export const a = 1;
+import { createContext } from "unctx";
+
+import { Unml } from "./types";
+
+const context = createContext<Unml>();
+
+const withEnsureContext = <A extends any[], T extends (...args: A) => any>(fn: T) => (...args: A): ReturnType<T> => {
+  if (!context.tryUse()) {
+    context.set(new Unml());
+  }
+  return fn(...args);
+};
+
+export const useUnml = withEnsureContext(context.use);
+export const tryUseUnml = withEnsureContext(context.tryUse);
---
+++ packages/kit/src/types.ts
@@ -0,0 +1,5 @@
+export class Unml {
+  getConfiguration() {}
+  getTab() {}
+  getView() {}
+}
