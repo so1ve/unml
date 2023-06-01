4e54145ffacb0fb42d6e8ad78711fdc837251e4a
so1ve <i@mk1.io>
2023-06-01 22:05:12 +0800

    test: PLAIN_PACKAGE_RE

--- packages/extensions/src/utils.test.ts
+++ packages/extensions/src/utils.test.ts
@@ -1,13 +1,21 @@
 import { describe, expect, it } from "vitest";
 
-import { EXTENSION_RE } from "./utils";
+import { EXTENSION_RE, PLAIN_PACKAGE_RE } from "./utils";
 
 describe("extensions / utils", () => {
-  it("utils", () => {
+  it("EXTENSION_RE", () => {
     expect(EXTENSION_RE.test("@scope/unml-extension-name")).toBeTruthy();
     expect(EXTENSION_RE.test("@scope/unml-extension")).toBeTruthy();
     expect(EXTENSION_RE.test("@scope/unml-extension-name-1")).toBeTruthy();
     expect(EXTENSION_RE.test("unml-extension-name")).toBeTruthy();
     expect(EXTENSION_RE.test("unml-extension")).toBeFalsy();
+  });
+
+  it("PLAIN_PACKAGE_RE", () => {
+    expect(PLAIN_PACKAGE_RE.test("@scope/unml-extension-name")).toBeFalsy();
+    expect(PLAIN_PACKAGE_RE.test("@scope/unml-extension")).toBeFalsy();
+    expect(PLAIN_PACKAGE_RE.test("@scope/unml-extension-name-1")).toBeFalsy();
+    expect(PLAIN_PACKAGE_RE.test("unml-extension-name")).toBeTruthy();
+    expect(PLAIN_PACKAGE_RE.test("unml-extension")).toBeTruthy();
   });
 });
