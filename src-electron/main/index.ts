7dbbd71683ca327eb116919a6fb01ddaf2cbae30
so1ve <i@mk1.io>
2023-07-04 22:02:55 +0800

    refactor: extract window initialization

--- src-electron/main/index.ts
+++ src-electron/main/index.ts
@@ -1,118 +1,23 @@
-import { release } from "node:os";
-import { join } from "node:path";
-
 import { createUnml, initUnml } from "@unml/core";
-import type { WebPreferences } from "electron";
-import { BrowserWindow, app, shell } from "electron";
+import { app } from "electron";
 import electronDebug from "electron-debug";
 
 import { loadExtensions } from "./extensions";
 import { loadHooks } from "./hooks";
 import { initProtocol, preInitProtocol } from "./protocol";
 import { initUi } from "./ui";
-
-process.env.DIST_ELECTRON = join(__dirname, "..");
-process.env.DIST = join(process.env.DIST_ELECTRON, "../dist");
-process.env.PUBLIC = process.env.VITE_DEV_SERVER_URL
-  ? join(process.env.DIST_ELECTRON, "../public")
-  : process.env.DIST;
-
-const TITLE = "UNML";
-const ICON = join(process.env.PUBLIC, "favicon.ico");
-const WIDTH = 1000;
-const HEIGHT = 750;
-const preload = join(__dirname, "../preload/index.js");
-const url = process.env.VITE_DEV_SERVER_URL;
-const indexHtml = join(process.env.DIST, "index.html");
-const WEB_PREFERENCES: WebPreferences = {
-  preload,
-  contextIsolation: true,
-};
-
-// Disable GPU Acceleration for Windows 7
-if (release().startsWith("6.1")) {
-  app.disableHardwareAcceleration();
-}
-
-// Set application name for Windows 10+ notifications
-if (process.platform === "win32") {
-  app.setAppUserModelId(app.getName());
-}
-
-if (!app.requestSingleInstanceLock()) {
-  app.quit();
-  process.exit(0);
-}
-
-let win: BrowserWindow | null = null;
-
-async function createWindow() {
-  win = new BrowserWindow({
-    title: TITLE,
-    icon: ICON,
-    width: WIDTH,
-    height: HEIGHT,
-    webPreferences: WEB_PREFERENCES,
-    resizable: false,
-    titleBarStyle: "hidden",
-    frame: false,
-  });
-
-  if (process.env.VITE_DEV_SERVER_URL) {
-    await win.loadURL(url);
-    win.webContents.openDevTools({
-      mode: "detach",
-    });
-  } else {
-    await win.loadFile(indexHtml);
-  }
-
-  win.webContents.setWindowOpenHandler(({ url }) => {
-    if (url.startsWith("https:")) {
-      shell.openExternal(url);
-    }
-
-    return { action: "deny" };
-  });
-}
-
-app.on("window-all-closed", () => {
-  win = null;
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
-
-app.on("second-instance", () => {
-  if (win) {
-    if (win.isMinimized()) {
-      win.restore();
-    }
-    win.focus();
-  }
-});
-
-app.on("activate", () => {
-  const allWindows = BrowserWindow.getAllWindows();
-  if (allWindows.length > 0) {
-    allWindows[0].focus();
-  } else {
-    createWindow();
-  }
-});
+import { createWindow } from "./window";
 
 // Initialization
 preInitProtocol();
 
-app.whenReady().then(startApp);
-
-async function startApp() {
+app.whenReady().then(async () => {
   initUnml(createUnml());
   initProtocol();
   await Promise.resolve()
     .then(createWindow)
-    .then(() => loadHooks({ win: win! }))
+    .then((win) => loadHooks({ win }))
     .then(loadExtensions)
     .then(initUi)
     .then(() => electronDebug());
-}
+});
---
+++ src-electron/main/window.ts
@@ -0,0 +1,97 @@
+import { release } from "node:os";
+import { join } from "node:path";
+
+import type { WebPreferences } from "electron";
+import { BrowserWindow, app, shell } from "electron";
+
+process.env.DIST_ELECTRON = join(__dirname, "..");
+process.env.DIST = join(process.env.DIST_ELECTRON, "../dist");
+process.env.PUBLIC = process.env.VITE_DEV_SERVER_URL
+  ? join(process.env.DIST_ELECTRON, "../public")
+  : process.env.DIST;
+
+const TITLE = "UNML";
+const ICON = join(process.env.PUBLIC, "favicon.ico");
+const WIDTH = 1000;
+const HEIGHT = 750;
+const preload = join(__dirname, "../preload/index.js");
+const url = process.env.VITE_DEV_SERVER_URL;
+const indexHtml = join(process.env.DIST, "index.html");
+const WEB_PREFERENCES: WebPreferences = {
+  preload,
+  contextIsolation: true,
+};
+
+// Disable GPU Acceleration for Windows 7
+if (release().startsWith("6.1")) {
+  app.disableHardwareAcceleration();
+}
+
+// Set application name for Windows 10+ notifications
+if (process.platform === "win32") {
+  app.setAppUserModelId(app.getName());
+}
+
+if (!app.requestSingleInstanceLock()) {
+  app.quit();
+  process.exit(0);
+}
+
+let win: BrowserWindow | null = null;
+
+app.on("window-all-closed", () => {
+  win = null;
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("second-instance", () => {
+  if (win) {
+    if (win.isMinimized()) {
+      win.restore();
+    }
+    win.focus();
+  }
+});
+
+app.on("activate", () => {
+  const allWindows = BrowserWindow.getAllWindows();
+  if (allWindows.length > 0) {
+    allWindows[0].focus();
+  } else {
+    createWindow();
+  }
+});
+
+export async function createWindow() {
+  win = new BrowserWindow({
+    title: TITLE,
+    icon: ICON,
+    width: WIDTH,
+    height: HEIGHT,
+    webPreferences: WEB_PREFERENCES,
+    resizable: false,
+    titleBarStyle: "hidden",
+    frame: false,
+  });
+
+  if (process.env.VITE_DEV_SERVER_URL) {
+    await win.loadURL(url);
+    win.webContents.openDevTools({
+      mode: "detach",
+    });
+  } else {
+    await win.loadFile(indexHtml);
+  }
+
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    if (url.startsWith("https:")) {
+      shell.openExternal(url);
+    }
+
+    return { action: "deny" };
+  });
+
+  return win;
+}
