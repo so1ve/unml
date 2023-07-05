233a818d7add90b3fd9a23d1348bc4964975e167
so1ve <i@mk1.io>
2023-07-05 13:55:20 +0800

    chore: cleanup

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>11</div>
   <iframe src="unml-resource://c/Workspace/unml/package.json"></iframe>
   <RouterLink to="./plugin/test">1111</RouterLink>
 </template>
