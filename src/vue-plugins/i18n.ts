4c0b85d558e6f9e4ddb8530caf1dfb4bb7e7c9b0
so1ve <i@mk1.io>
2023-07-06 20:03:11 +0800

    refactor: import messages from unplugin

--- src/vue-plugins/i18n.ts
+++ src/vue-plugins/i18n.ts
@@ -1,14 +1,5 @@
+import messages from "@intlify/unplugin-vue-i18n/messages";
 import { createI18n } from "vue-i18n";
-
-const messages = Object.fromEntries(
-  Object.entries(
-    import.meta.glob<{ default: any }>("../locales/*.y(a)?ml", { eager: true }),
-  ).map(([key, value]) => {
-    const yaml = key.endsWith(".yaml");
-
-    return [key.slice(14, yaml ? -5 : -4), value.default];
-  }),
-);
 
 const i18n = createI18n({
   legacy: false,
