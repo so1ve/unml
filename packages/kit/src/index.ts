cc64aebd59b7119dde13096a30b52edbe4692ef7
so1ve <i@mk1.io>
2025-12-27 02:39:36 +0800

    init kit

--- packages/kit/src/index.ts
+++ packages/kit/src/index.ts
@@ -1,1 +1,45 @@
-export const a = 1;
+import { createContext } from "unctx";
+
+export interface CommandRegistry {
+  register: (id: string, handler: (...args: any[]) => any) => void;
+  execute: (id: string, ...args: any[]) => Promise<any>;
+}
+
+export interface MenuRegistry {
+  // Menus are primarily defined in package.json, but dynamic manipulation can be added here
+}
+
+export interface UIManager {
+  // UI related operations
+}
+
+export interface RPCManager {
+  handle: (command: string, handler: (args: any) => any) => void;
+  call: (command: string, args: any) => Promise<any>;
+}
+
+export interface ServiceManager {
+  register: (id: string, service: any) => void;
+  get: <T = any>(id: string) => T | undefined;
+}
+
+export interface UnmlAPI {
+  commands: CommandRegistry;
+  menus: MenuRegistry;
+  ui: UIManager;
+  rpc: RPCManager;
+  services: ServiceManager;
+}
+
+const unmlCtx = createContext<UnmlAPI>();
+
+export const useUnml: () => UnmlAPI = unmlCtx.use;
+export const provideUnml: (value: UnmlAPI, replace?: boolean) => void =
+  unmlCtx.set;
+
+export interface Plugin {
+  setup: () => void | Promise<void>;
+  dispose?: () => void | Promise<void>;
+}
+
+export const definePlugin = (plugin: Plugin): Plugin => plugin;
