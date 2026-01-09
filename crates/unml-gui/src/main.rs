db4adaecd5c0ff930231cc9509ecce8af18f40d6
so1ve <i@mk1.io>
2026-01-09 23:59:37 +0800

    add window title

--- crates/unml-gui/src/main.rs
+++ crates/unml-gui/src/main.rs
@@ -15,17 +15,24 @@ fn main() {
 
         let bounds = Bounds::centered(None, size(px(960.), px(600.)), cx);
 
-        cx.open_window(
-            WindowOptions {
-                window_decorations: Some(WindowDecorations::Client),
-                titlebar: Some(gpui_component::TitleBar::title_bar_options()),
-                window_bounds: Some(WindowBounds::Windowed(bounds)),
-                is_resizable: false,
-                ..Default::default()
-            },
-            |_, cx| cx.new(|_| app::LauncherView::new()),
-        )
-        .unwrap();
+        let window = cx
+            .open_window(
+                WindowOptions {
+                    window_decorations: Some(WindowDecorations::Client),
+                    titlebar: Some(gpui_component::TitleBar::title_bar_options()),
+                    window_bounds: Some(WindowBounds::Windowed(bounds)),
+                    is_resizable: false,
+                    ..Default::default()
+                },
+                |_, cx| cx.new(|_| app::LauncherView::new()),
+            )
+            .unwrap();
+
+        window
+            .update(cx, |_, window, _cx| {
+                window.set_window_title("UNML");
+            })
+            .expect("failed to update window");
 
         cx.activate(true);
     });
